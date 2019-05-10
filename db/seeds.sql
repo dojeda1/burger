4f2e464145bb43f95ad8e6662bd31bb6adf60d88
Dominic Ojeda <dominicojeda.coding@gmail.com>
2019-05-09 19:52:17 -0600

    Basic file structure created.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("bacon cheese burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("spicy jalepeno burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("mushroom swiss burger", FALSE);
