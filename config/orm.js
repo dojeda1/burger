5d78304868bf2b805f542e41d4a50b096b46f015
Dominic Ojeda <dominicojeda.coding@gmail.com>
2019-05-09 20:18:08 -0600

    Orm functions created.

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,37 @@
-var connection = require("./connection.js")
+var connection = require("./connection.js");
+
+var orm = {
+    selectAll: function (table, callback) {
+        var queryString = "SELECT * FROM ??;";
+        connection.query(queryString, [table], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            console.log(result);
+            callback(result);
+        });
+    },
+    insertOne: function (table, col, value) {
+        var queryString = "INSERT INTO ?? (??, devoured) VALUES (?, FALSE);";
+        connection.query(queryString, [table, col, value], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            console.log(result);
+            callback(result);
+        });
+    },
+    updateOne: function (table, col, value, id) {
+        var queryString = "UPDATE ?? SET ?? = ? WHERE (id = ?);";
+        connection.query(queryString, [table, col, value, id], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            console.log(result);
+            callback(result);
+        });
+    }
+};
+
+
+module.exports = orm;
