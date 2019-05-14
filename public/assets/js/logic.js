7c3a5b61b4982e83e71cadc7f75e925537dc96cd
Dominic Ojeda <dominicojeda.coding@gmail.com>
2019-05-14 14:02:26 -0600

    burger can now be eaten. Chages devoured state in database and sorts accordingly in html page.

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -1,23 +1,41 @@
 console.log("hiya")
 
-$("img").on("click", function () {
+$(function () {
 
-    console.log("hey")
+    $("#add-burger-btn").on("click", function () {
+        var newBurger = {
+            name: $("#burgerForm").val().trim(),
+        };
+        console.log(newBurger);
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("created new burger");
+                location.reload();
+            }
+        );
+    });
 
-    // var newBurger = {
-    //     name: $("#burgerForm").val().trim(),
-    // };
+    $(".eat-btn").on("click", function () {
+        var id = $(this).data("id");
 
-    // console.log(newBurger);
+        var eatenState = {
+            devoured: 1
+        };
 
-    // $.ajax("/api/burgers", {
-    //     type: "POST",
-    //     data: newBurger
-    // }).then(
-    //     function () {
-    //         console.log("created new burger");
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: eatenState
+        }).then(
+            function () {
+                console.log("changed devoured to true");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
 
-    //         location.reload();
-    //     }
-    // );
 });
